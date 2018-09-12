a957f21d44ae5c67bf211496166381d9079a2e55
Margreet Dijkstra <dijkstra.margreet@gmail.com>
2018-09-12 19:55:39 +0100

    ✨added homepage - everhthig on one page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get '/users/homepage', to: 'users#homepage', as: 'homepage'
-  get '/users/portfolio', to: 'users#portfolio', as: 'portfolio'
-  get '/users/contact', to: 'users#contact', as: 'contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#homepage'
 end
